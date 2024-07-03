0cbabe513526ef56f90a9b598a588afb05d9e780
zziwonCHOI <wldnjssem6@naver.com>
2024-07-03 15:48:59 +0900

    bulid: 초기빌드

---
+++ portfolio/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/a.scss'
+
+const Main = () => {
+    
+    return (
+        <div className='a'>
+            <p id='a'></p>
+        </div>
+    );
+};
+
+export default Main;
