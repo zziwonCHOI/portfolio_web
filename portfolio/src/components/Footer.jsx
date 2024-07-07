e66ab5d9e8219a43475ce785cbebb640643fbabf
zziwonCHOI <wldnjssem6@naver.com>
2024-07-07 20:04:23 +0900

    feat: footer section

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../assets/styles/footer.scss'
+
+const Footer = () => {
+    return (
+        <div className='footer_container'>
+            <div className='text1'>
+                <p>copyright 2024. Designed & Developed by JIWON CHOI</p>
+                <div className='btn'>
+                    <p>Github</p>
+                    <p>Blog</p>
+                    <p>Email</p>
+                </div>
+                <p>Do you like my portfolio? Send email!</p>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
