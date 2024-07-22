090ded2192ae9f0f9129abefdc246a236cc29468
zziwonCHOI <wldnjssem6@naver.com>
2024-07-22 23:31:14 +0900

    feat: 배경 별 애니메이션 추가

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Intro from './Intro';
 import '../assets/styles/main.scss'
 import Skils from './Skils';
@@ -6,9 +6,36 @@ import Projects from './Projects';
 import Footer from './Footer';
 
 const Main = () => {
-    
+    const [star,setStar]=useState([]);
+    useEffect(()=>{
+        const createStars=()=>{
+            let starsArray=[];
+            for(let i=0;i<100;i++){
+                let star={
+                    id:i,
+                    left:Math.random()*100+'vw', //별의 수평 위치 설정
+                    animationDuration:Math.random()*50+5+'s', //지속 시간
+                    animationDelay:Math.random()*5+'s',//시작 전 대기 시간
+                }
+                starsArray.push(star)
+            }
+            setStar(starsArray);
+        }
+        createStars()
+    },[])
     return (
         <div className='main_container'>
+            {star.map((s)=>(
+                <div
+                key={s.id}
+                className='star'
+                style={{
+                    left:s.left,
+                    animationDuration:s.animationDuration,
+                    animationDelay:s.animationDelay
+                }}
+                ></div>
+            ))}
             <Intro/>
             <Skils/>
             <Projects/>
