91805c1c6833e915e7095a9cb465e20e48d18f0a
zziwonCHOI <wldnjssem6@naver.com>
2024-07-22 23:31:52 +0900

    feat: 프로필 사진 hover시에 정보 나오게 하기

--- portfolio/src/components/Intro.jsx
+++ portfolio/src/components/Intro.jsx
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/intro.scss'
 import Jw from '../assets/images/jw.PNG'
+import Typewriter from 'typewriter-effect';
+
 const Intro = () => {
+    const [img, setImg]=useState(true);
+    const handlerMouseHover=()=>{
+        setImg(false)
+    }
+    const handlerMouseLeave=()=>{
+        setImg(true)
+    }
     return (
         <div className='intro_container'>
             <div className='title'>
-                <p>JIWON CHOI</p>
-                <p>Front-end Developer</p>
+            <Typewriter
+                options={{
+                strings: ['JIWON CHOI'],
+                autoStart: true,
+                loop: true,
+                pauseFor:5000
+                }}
+            />
+            <Typewriter
+                options={{
+                strings: ['Front-end Developer'],
+                autoStart: true,
+                loop: true,
+                pauseFor:5000
+                }}
+            />
             </div>
             <div className='myself'>
-                <img src={Jw} alt='jw'></img>
+                <div onMouseEnter={handlerMouseHover} onMouseLeave={handlerMouseLeave}>
+                    {img?<img src={Jw} alt='jw'></img>:
+                        <div className='img_info'>
+                            <p style={{fontFamily:'neodgm'}}>최지원</p>
+                            <p style={{fontFamily:'"Black Ops One", system-ui'}}>2002.01.22</p>
+                            <p style={{fontFamily:'"Black Ops One", system-ui'}}>Myongji Univ</p>
+                        </div>
+                    }
+                </div>
                 <div className='texts'>
-                    <p>상상한 무궁무진한 아이디어를 제가 배운 기술로 구현해가는 과정이 재밌습니다. 
-                        항상 새로운 시도를 두려워하지 않는 프론트엔드 개발자가 되겠습니다.<br/></p>
-                    <p>“smooth waters run deep” 제가 가장 좋아하는 영어 격언 입니다. 
-                        거칠고 빠르게 지나가는 물보다 잔잔하게 오랫동안 깊이 있게 흐르는 물처럼 꾸준히 공부하고, 계속 해서 나아갈 자신이 있습니다. 
-                        성실하게 쌓은 지식으로, 회사에 실질적인 도움이 되는 프론트엔드 개발자로 일하고 싶습니다.</p>
+                    <p>새로운 것을 받아들이고 도전하는 것을 즐기는 사람으로써, 변화에 유연하게 대처하는 
+                        프론트엔드가 되겠습니다.
+                        <br/></p>
+                    <p>“If you have mastered your profession but lack the ability to be persistent, 
+                        you will not succeed... You are a failure when you stop trying again and again.”
+                        -Dozie Uzochukwu <br/><br/>
+                        제가 매일 되새기는 명언입니다. 끊임없이 공부하고, 연구하는 개발자가 되고자 노력하고 있습니다. 
+                        </p>
                 </div>
             </div>
         </div>
