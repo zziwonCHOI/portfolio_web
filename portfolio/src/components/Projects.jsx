edab176236a1ab145bd97785c80cca4ec491d609
zziwonCHOI <wldnjssem6@naver.com>
2024-07-23 23:15:45 +0900

    chore: 프로젝트 section 디자인, 동적 요소 수정

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/project.scss'
 import time from '../assets/images/timelogo.png'
 import mcc from '../assets/images/logo.png'
+import git from '../assets/images/github.png'
+import link from '../assets/images/link.png'
 const Projects = () => {
     return (
         <div className='project_container'>
@@ -21,6 +23,7 @@ const Projects = () => {
                         <p>Typescript</p>
                         <p>React Native</p>
                         <p>Redux</p>
+                        <a href='https://github.com/capstone-team6/frontend'><img src={git} style={{width:'3vw', marginTop:'0.5vw', marginLeft:'1vw'}}></img></a>
                     </div>
                 </div>
             </div>
@@ -28,7 +31,7 @@ const Projects = () => {
                 <div className='project_title'>
                     <div className='icon'>
                         <p id='name'>M.C.C homepage</p>
-                        <img src={mcc} alt='icon'/>
+                        <img src={mcc} alt='icon' style={{height:'18vw'}}/>
                     </div>
                     <p id='ex'>명지대학교 컴퓨터 동아리 m.c.c의 홈페이지. 동아리의 간략한 소개와 활동 결과들을 보여주는 
                         홈페이지
@@ -36,6 +39,8 @@ const Projects = () => {
                     <div className='skil'>
                         <p>Javascript</p>
                         <p>React</p>
+                        <a href='https://github.com/MJU-MCC/MCC_homepage'><img src={git} style={{width:'3vw', marginTop:'0.5vw', marginLeft:'1vw'}}></img></a>
+                        <a href='http://mju-mcc.com'><img src={link} style={{width:'3vw', marginTop:'0.5vw', marginLeft:'1vw'}}></img></a>
                     </div>
                 </div>
             </div>
